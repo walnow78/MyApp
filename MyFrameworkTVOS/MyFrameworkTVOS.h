83fdafb6f6fcd63c4c6ee0ade9ce85ca3bc2fbf7
Pawel Walicki <pawel.walicki@cognizant.com>
2018-04-09 16:39:25 +0200

    Add tvOS framework

---
+++ MyFrameworkTVOS/MyFrameworkTVOS.h
@@ -0,0 +1,19 @@
+//
+//  MyFrameworkTVOS.h
+//  MyFrameworkTVOS
+//
+//  Created by Walicki, Pawel (Cognizant) on 4/9/18.
+//  Copyright © 2018 Pawel Walicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFrameworkTVOS.
+FOUNDATION_EXPORT double MyFrameworkTVOSVersionNumber;
+
+//! Project version string for MyFrameworkTVOS.
+FOUNDATION_EXPORT const unsigned char MyFrameworkTVOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFrameworkTVOS/PublicHeader.h>
+
+
